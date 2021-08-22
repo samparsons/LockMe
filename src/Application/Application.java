e2df782df0e8bce7287e639504788b2a9ba0e780
samparsons <sparsons.llc@gmail.com>
2021-08-22 03:29:54 +0000

    LockMe welcome screen

---
+++ src/Application/Application.java
@@ -0,0 +1,56 @@
+package Application;
+
+public class Application {
+
+	public static void main(String[] args) {
+		welcomeScreen();
+
+	}
+	
+	public static void welcomeScreen() {
+		System.out.println("      ___       ___           ___           ___           ___           ___     ");
+		System.out.println("     /\\__\\     /\\  \\         /\\  \\         /\\__\\         /\\__\\         /\\  \\    ");
+		System.out.println("    /:/  /    /::\\  \\       /::\\  \\       /:/  /        /::|  |       /::\\  \\   ");
+		System.out.println("   /:/  /    /:/\\:\\  \\     /:/\\:\\  \\     /:/__/        /:|:|  |      /:/\\:\\  \\  ");
+		System.out.println("  /:/  /    /:/  \\:\\  \\   /:/  \\:\\  \\   /::\\__\\____   /:/|:|__|__   /::\\~\\:\\  \\ ");
+		System.out.println(" /:/__/    /:/__/ \\:\\__\\ /:/__/ \\:\\__\\ /:/\\:::::\\__\\ /:/ |::::\\__\\ /:/\\:\\ \\:\\__\\");
+		System.out.println(" \\:\\  \\    \\:\\  \\ /:/  / \\:\\  \\  \\/__/ \\/_|:|~~|~    \\/__/~~/:/  / \\:\\~\\:\\ \\/__/");
+		System.out.println("  \\:\\  \\    \\:\\  /:/  /   \\:\\  \\          |:|  |           /:/  /   \\:\\ \\:\\__\\  ");
+		System.out.println("   \\:\\  \\    \\:\\/:/  /     \\:\\  \\         |:|  |          /:/  /     \\:\\ \\/__/  ");
+		System.out.println("    \\:\\__\\    \\::/  /       \\:\\__\\        |:|  |         /:/  /       \\:\\__\\    ");
+		System.out.println("     \\/__/     \\/__/         \\/__/         \\|__|         \\/__/         \\/__/    ");
+		System.out.println(" Developed by Sam Parsons");
+		System.out.println(" Class: Phase-1: Implement OOPS using JAVA with Data Structures and Beyond");
+		System.out.println(" Due Date: Aug 29, 2021");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("             ____________________");
+		System.out.println("            /\\  _______  _______ \\");
+		System.out.println("           /  \\ \\     /\\ \\     /\\ \\");
+		System.out.println("          / /\\ \\ \\   /  \\ \\   /  \\ \\");
+		System.out.println("         / /  \\ \\ \\ /    \\ \\ /    \\ \\");
+		System.out.println("        / /    \\ \\ \\______\\ \\______\\ \\");
+		System.out.println("       / /______\\ \\  _______  _______ \\");
+		System.out.println("      /  \\      /  \\ \\     /\\ \\     /\\ \\");
+		System.out.println("     / /\\ \\    / /\\ \\ \\   /  \\ \\   /  \\ \\");
+		System.out.println("    / /  \\ \\  / /  \\ \\ \\ /    \\ \\ /    \\ \\");
+		System.out.println("   / /    \\ \\/ /    \\ \\ \\______\\ \\______\\ \\");
+		System.out.println("  / /______\\  /______\\ \\___________________\\");
+		System.out.println("  \\ \\      /  \\      / / ______   ______   /");
+		System.out.println("   \\ \\    / /\\ \\    / / /\\     / /\\     / /");
+		System.out.println("    \\ \\  / /  \\ \\  / / /  \\   / /  \\   / /");
+		System.out.println("     \\ \\/ /    \\ \\/ / /    \\ / /    \\ / /");
+		System.out.println("      \\  /______\\  / /______/ /______/ /");
+		System.out.println("       \\ \\      / / _______  _______  /");
+		System.out.println("        \\ \\    / / /      / /      / /");
+		System.out.println("         \\ \\  / / / \\    / / \\    / /");
+		System.out.println("          \\ \\/ / /   \\  / /   \\  / /");
+		System.out.println("           \\  / /_____\\/ /_____\\/ /");
+		System.out.println("            \\/___________________/LGB");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("------------------------------------------------");
+		System.out.println(" Boxes by Richard Kirk");
+	}
+
+}
